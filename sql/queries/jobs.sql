4eacd8da29d9a62cbba2e8a240e17e5ee6a375a2
Matthew <mattyoungjnr@gmail.com>
2025-07-29 12:38:26 +0000

    feat(db): add job status/duration update and GetJobById query

--- sql/queries/jobs.sql
+++ sql/queries/jobs.sql
@@ -18,6 +18,12 @@ VALUES(
 
 RETURNING id, type, payload, status, run_at, created_at;
 
+-- name: GetJobById :one 
+SELECT id, type, payload, status, retries, run_at, created_at 
+FROM jobs
+RETURNING id, type, payload, status, retries, run_at, created_at;
+
+
 -- name: DeleteJobById :exec
 DELETE FROM jobs 
 WHERE id = $1;
@@ -33,3 +39,15 @@ SELECT id, type, payload, status, run_at, created_at
 FROM jobs 
 ORDER BY created_at DESC
 LIMIT $1 OFFSET $2;
+
+
+-- name: UpdateJobStatus :exec
+UPDATE jobs
+SET status = $1
+WHERE id = $2;
+
+
+-- name: UpdateJobDuration :exec
+UPDATE jobs
+SET duration_ms = $2
+WHERE id = $1;
