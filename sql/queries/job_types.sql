6dafb0867adbada6fa35e570195ddb3e07b099b9
Matthew <mattyoungjnr@gmail.com>
2025-07-29 12:54:48 +0000

    feat(db): add queries for managing job types

---
+++ sql/queries/job_types.sql
@@ -0,0 +1,19 @@
+-- name: CreateJobType :many
+INSERT INTO job_types(id, name)
+VALUES
+('send_email'), 
+('send_push_notification'), 
+('send_sms')
+RETURNING id, name;  
+
+
+-- name: ListJobTypes :many
+SELECT id, name 
+FROM job_types
+ORDER BY name;
+
+
+-- name: GetJobTypeById :one
+SELECT id, name 
+FROM job_types
+WHERE id = $1;
